1a3228b618bd74b9bb212e4f55bf47f607899d66
katie <katiepb525@gmail.com>
2023-01-13 14:04:42 -0600

    complete 11a/11b, skip 11c

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,22 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject(:felix) {described_class.new('Felix', 'Domestic Longhair')}
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'when method name is the same in multiple classes' do
+    it 'will respond to talk' do
+      expect(felix).to respond_to(:talk)
+    end
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(felix).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(felix).to be_hiding
   end
 end
